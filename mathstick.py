0b0da93818aa2e2f555d08339377b9908feff7d4
kostaskoro99 <t8230062@aueb.gr>
2026-06-13 01:01:24 +0300

    Update mathstick.py

--- mathstick.py
+++ mathstick.py
@@ -10,3 +10,12 @@ LAYOUT = {
     '6': {0, 1, 3, 4, 5, 6}, '7': {1, 2, 3}, '8': {0, 1, 2, 3, 4, 5, 6},
     '9': {0, 1, 2, 3, 4, 6}
 }
+
+class MatchstickSolver:
+    def __init__(self, problem, max_k):
+        self.problem = problem
+        self.max_k = max_k
+        self.nodes_visited = 0
+        self.nodes_pruned = 0
+        self.solutions = {str(i): [] for i in range(1, max_k + 1)}
+        self.tokens = list(problem.replace(" ", ""))
